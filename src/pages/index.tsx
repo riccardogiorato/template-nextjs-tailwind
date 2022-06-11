cd82522b3b73c408560a400b7bf659203a15260c
Riccardo Giorato <riccardo@teero.com>
2022-06-11 12:28:40 +0200

    update packages and .vscode support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,52 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-white shadow p-3 rounded lg:w-64">
-      <div>
-        <div className="bg-cover bg-center bg-gray-300 h-32 rounded"></div>
-      </div>
-      <div className="mt-6">
-        <p className="text-lg text-bold tracking-wide text-gray-600 mb-2">
-          Title
-        </p>
-        <p className="text-sm text-gray-600 font-hairline">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-      </div>
-      <div className="mt-6">
-        <button className="rounded shadow-md flex items-center shadow bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
-          Default
-        </button>
+    <div className="my-auto max-w-2xl mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+      <img
+        className="object-cover w-full h-64"
+        src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        alt="Article"
+      />
+
+      <div className="p-6">
+        <div>
+          <span className="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">
+            Product
+          </span>
+          <a
+            href="#"
+            className="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-200 transform dark:text-white hover:text-gray-600 hover:underline"
+          >
+            I Built A Successful Blog In One Year
+          </a>
+          <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+            parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris
+            egestas quam volutpat viverra. In pretium nec senectus erat. Et
+            malesuada lobortis.
+          </p>
+        </div>
+
+        <div className="mt-4">
+          <div className="flex items-center">
+            <div className="flex items-center">
+              <img
+                className="object-cover h-10 rounded-full"
+                src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60"
+                alt="Avatar"
+              />
+              <a
+                href="#"
+                className="mx-2 font-semibold text-gray-700 dark:text-gray-200"
+              >
+                Jone Doe
+              </a>
+            </div>
+            <span className="mx-1 text-xs text-gray-600 dark:text-gray-300">
+              1 SEP 2022
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
