cf62decbf92e30e430913f48d20e7e2f6c5d4fc1
Riccardo Giorato <riccardo@teero.com>
2023-09-16 12:19:28 +0200

    add prisma and migrate tailwind config from js to typescript

---
+++ src/prisma.ts
@@ -0,0 +1,13 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+export const prisma: PrismaClient =
+  (global as any).prisma ||
+  new PrismaClient({
+    log: ["error"],
+  });
+
+export * from "@prisma/client";
+
+if (process.env.NODE_ENV !== "production") {
+  (global as any).prisma = prisma;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
