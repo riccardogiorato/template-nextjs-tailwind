7eeeaa07a30d6e8f8d0384ae2039bce0bf9e39fb
Riccardo Giorato <riccardo@teero.com>
2023-11-19 16:51:06 +0100

    change article layout from ghost casper

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,37 +6,33 @@ interface NavbarProps {
 
 export const Navbar: FC<NavbarProps> = ({ pages }) => {
   return (
-    <nav className="flex" aria-label="Breadcrumb">
-      <ol role="list" className="flex items-center space-x-4">
-        <li>
-          <div>
-            <a href="/" className="text-gray-400 hover:text-gray-500">
-              Home
-            </a>
-          </div>
-        </li>
-        {pages.map((page) => (
-          <li key={page.name}>
-            <div className="flex items-center">
-              <svg
-                className="h-5 w-5 flex-shrink-0 text-gray-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                aria-hidden="true"
-              >
-                <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
-              </svg>
-              <a
-                href={page.href}
-                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                aria-current={page.current ? "page" : undefined}
-              >
-                {page.name}
-              </a>
-            </div>
-          </li>
-        ))}
-      </ol>
+    <nav className="bg-gray-900 p-4 mt-0 w-full">
+      <div className="container mx-auto flex items-center">
+        <div className="flex text-white font-extrabold">
+          <a
+            className="flex text-white text-base no-underline hover:text-white hover:no-underline"
+            href="/"
+          >
+            <span className="hidden w-0 md:w-auto md:block pl-1">Homepage</span>
+          </a>
+        </div>
+        <div className="flex pl-4 text-sm">
+          <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
+            {pages.map((page) => (
+              <li className="mr-2" key={page.name}>
+                <a
+                  className={`inline-block py-2 px-2 text-white no-underline ${
+                    page.current ? "font-bold" : ""
+                  }`}
+                  href={page.href}
+                >
+                  {page.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
